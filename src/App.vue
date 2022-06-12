a1e5ef2706e261df4c21da59bb7d339bc44c7a7f
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2022-06-12 17:29:45 -0500

    Main | Añadiendo categorias y renderizando componentes cards, añadiendo objetos a data.js

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <!-- <div id="app"> -->
     <v-app id="app">
       <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Inicio</router-link> |
+      <router-link to="/libros">Libros</router-link>
     </div>
     <router-view/>
     </v-app>
